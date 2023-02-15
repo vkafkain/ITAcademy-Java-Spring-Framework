23dc740afae36f3a4703a20b8f2ca4b60c840573
Víctor López <vlopezmoles@gmail.com>
2023-02-15 09:26:10 +0100

    init redactor

---
+++ Sprint1/src/TascaS101/n3exercici1/Redactor.java
@@ -0,0 +1,12 @@
+package TascaS101.n3exercici1;
+
+public class Redactor {
+    private String nom;
+    private final String DNI;
+    private static final int sou = 1500;
+
+    public Redactor(String nom, String dni, int sou) {
+        this.nom = nom;
+        DNI = dni;
+    }
+}
